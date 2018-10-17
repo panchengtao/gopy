f77fc103812b3fa27a548e509ce2786ea4594c49
panchengtao <pan.chengtao@synyi.com>
2018-10-17 15:33:28 +0800

    style: update annotation and error info

--- go-python.h
+++ go-python.h
@@ -2,11 +2,9 @@
 #include "frameobject.h"
 #include "marshal.h"
 
-/* stdlib */
 #include <stdlib.h>
 #include <string.h>
 
-/* go-python */
 #define _gopy_max_varargs 8 /* maximum number of varargs accepted by go-python */
 
 int
